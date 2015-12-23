910d458db2daa1cf8fd98f774878acd7b05b809d
Stanislav Trifan <trifanstanislav@gmail.com>
2015-12-23 23:29:14 +0200

    Chenged BDD report plugin

--- service/src/test/java/features/FeaturesRunner.java
+++ service/src/test/java/features/FeaturesRunner.java
@@ -8,6 +8,6 @@ import org.junit.runner.RunWith;
  * Created by stanislav on 10/27/15.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "json:src/test/resources/bdd_report/cucumber-report.json"})
+@CucumberOptions(plugin = {"pretty", "json:target/site/documentation/cucumber-report.json"})
 public class FeaturesRunner {
 }
