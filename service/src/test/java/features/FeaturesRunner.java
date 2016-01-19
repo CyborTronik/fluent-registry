3c83f806e3b44abc515d65ae454e84fc458d9b91
Stanislav Trifan <trifanstanislav@gmail.com>
2016-01-19 18:02:12 +0200

    Updated BDD report generation

--- service/src/test/java/features/FeaturesRunner.java
+++ service/src/test/java/features/FeaturesRunner.java
@@ -8,6 +8,6 @@ import org.junit.runner.RunWith;
  * Created by stanislav on 10/27/15.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "json:target/site/documentation/cucumber-report.json"})
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber", "json:target/site/documentation/cucumber-report.json"})
 public class FeaturesRunner {
 }
